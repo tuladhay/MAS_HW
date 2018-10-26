2b683d4eb87a6bb770e3c522307a76bdb84cebcc
Yathartha <tuladhay@oregonstate.edu>
2018-10-26 11:44:28 -0700

    implementing G

--- HW3/environment.py
+++ HW3/environment.py
@@ -1,34 +1,29 @@
 import numpy as np
+from collections import Counter
+import matplotlib.pyplot as plt
 
-
-class Agent:
-    def __init__(self, n_nights):
-        # Action is the night to choose to go to the bar
-        self.n_actions = n_nights
-        self.value_table = 100*np.random.uniform(self.n_actions)
-
-    def choose_action(self):
-        # choose the index of the max action in the value table
-        pass
-
-    def update_value_table(self, reward):
-        # Here, the reward can be G,D or Local
-        pass
-
-
-class Elfabor:
-    def __init__(self, n_agents, n_nights, b, alpha):
-        # Total number of agents
+class Agents:
+    def __init__(self, n_agents, n_nights, b):
         self.n_agents = n_agents
-        # K nights that an agent can choose to attend the bar
-        self.n_actions = n_nights
-        # optimal number of attendees
+        self.n_nights = n_nights
         self.b = b
-        # learning rate
-        self.alpha = alpha
+        self.gamma = 0.1
+        self.value_table = np.random.uniform(0, 1, [self.n_agents, self.n_nights])
+        self.chosen_actions = []
+
+    def select_actions(self):
+        self.chosen_actions = np.argmax(self.value_table, 1)  # night that was chosen by each agent. size=n_agents
+        # TODO: add epsilon-greedy
 
     def get_global_reward(self):
-        pass
+        # Calculate how many times each night was chosen by the agents
+        nights_selected_dict = Counter(self.chosen_actions)
+        global_reward = 0
+        for k in range(self.n_nights):
+            reward = nights_selected_dict[k]*np.exp(-nights_selected_dict[k]/self.b)
+            global_reward += reward
+        # return equally distributed reward
+        return global_reward*np.ones(self.n_agents)/self.n_agents
 
     def get_local_reward(self):
         pass
@@ -36,8 +31,31 @@ class Elfabor:
     def get_difference_reward(self):
         pass
 
+    def update_value_table(self, agent_reward):
+        # agent_reward is a list with reward for each agent
+        # the update has to depend on the action taken, and the agent_reward
+        for agent in range(self.n_agents):
+            self.value_table[agent, self.chosen_actions[agent]] += self.gamma*agent_reward[agent]
+
     def reset(self):
         pass
 
-    def step(self):
-        pass
+
+if __name__=="__main__":
+    # Parameters
+    n_agents = 5
+    n_nights = 6
+    b = 2
+    n_episodes = 1000
+    episode_returns = []
+
+    for episode in range(n_episodes):
+        agents = Agents(n_agents, n_nights, b)
+        agents.select_actions()
+        # Get reward for each agent as a list
+        rewards = agents.get_global_reward()
+        episode_returns.append(np.sum(rewards))  # this is the total rewards for all agents
+        agents.update_value_table(rewards)
+
+    plt.plot(episode_returns)
+    plt.show()
